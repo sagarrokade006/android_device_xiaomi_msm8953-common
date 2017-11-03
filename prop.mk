140fc74bbac03380ddf3c3db67c120c1cbada3a1
Bharath Gopal <gopalb@codeaurora.org>
2017-11-03 12:37:45 +0530

    msm8953-common: Add property to enable display default color mode
    Add display default color mode property with value of 1.
    
    Change-Id: Ie73673a00e9e569a0c961394ab2759cd20d82418
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- prop.mk
+++ prop.mk
@@ -151,7 +151,7 @@ persist.rild.nitz_short_ons_3=""
 
 # Property to enable display default color mode
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.display.enable_default_color_mode=0
+vendor.display.enable_default_color_mode=1
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
