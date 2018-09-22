235876ae6a181a5e353b625db2a21113019987e1
kondors1995 <normandija1945@gmail.com>
2018-09-22 21:06:38 +0000

    Disable Clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,9 +46,9 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_CLANG_COMPILE := true
+#TARGET_KERNEL_CLANG_COMPILE := true
 #TARGET_KERNEL_CLANG_VERSION := 7.0.2
-TARGET_KERNEL_CLANG_PATH := ~/syberia/prebuilts/clang/host/linux-x86/clang-4691093/bin/
+#TARGET_KERNEL_CLANG_PATH := ~/syberia/prebuilts/clang/host/linux-x86/clang-4691093/bin/
 
 # ANT
 # BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
