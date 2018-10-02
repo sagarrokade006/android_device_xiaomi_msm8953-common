5a7730db03fd28f0e23348fc4ba90a72a22ce770
kondors1995 <normandija1945@gmail.com>
2018-10-02 15:14:19 +0000

    Build OTA

--- msm8953.mk
+++ msm8953.mk
@@ -411,3 +411,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/move_wifi_data.sh:system/bin/move_wifi_data.sh \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# OTA Updates
+PRODUCT_PACKAGES += \
+	Updates
