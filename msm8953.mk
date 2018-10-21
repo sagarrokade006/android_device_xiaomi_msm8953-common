f0ef19f519599d01d1aaa4a301e7e4448478aa22
kondors1995 <normandija1945@gmail.com>
2018-10-21 17:50:00 +0300

    Revert "Add spectrum support"
    This reverts commit a478c42b2a0d0418cfce1a2c2446f5993f96178f.

--- msm8953.mk
+++ msm8953.mk
@@ -282,9 +282,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    init.spectrum.rc \
-    init.spectrum.sh
+    ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
